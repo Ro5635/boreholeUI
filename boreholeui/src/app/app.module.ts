1b6c5174e6b8e203cb3086208d09c9056a097a0e
Robert Curran <ro5635@gmail.com>
2019-02-05 20:51:56 +0000

    Added Angular Material to project along with guesture support  (hammer.js) and material icons.
     On branch master
     Changes to be committed:
    	modified:   boreholeui/package-lock.json
    	modified:   boreholeui/package.json
    	modified:   boreholeui/src/app/app.module.ts
    	modified:   boreholeui/src/index.html
    	modified:   boreholeui/src/main.ts
    	modified:   boreholeui/src/styles.scss

--- boreholeui/src/app/app.module.ts
+++ boreholeui/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrowserAnimationsModule
   ],
   imports: [
     BrowserModule,
